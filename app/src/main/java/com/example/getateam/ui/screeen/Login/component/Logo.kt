87f46e77d01b3c0647fe7358a4d91f57fc50703f
fprtmjinho <fjrtmjinho@gmail.com>
2026-03-22 18:16:46 +0900

    [feat] 로고 추가

--- app/src/main/java/com/example/getateam/ui/screeen/Login/component/Logo.kt
+++ app/src/main/java/com/example/getateam/ui/screeen/Login/component/Logo.kt
@@ -1,22 +1,20 @@
 package com.example.getateam.ui.screeen.Login.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.*
 import androidx.compose.ui.unit.dp
+import com.example.getateam.R
 
 @Composable
 fun Logo() {
-    Box(
-        modifier = Modifier
-            .size(100.dp)
-            .background(Color.LightGray),
-        contentAlignment = Alignment.Center
-    ) {
-        Text("LOGO")
-    }
+    Image(
+        painter = painterResource(id = R.drawable.logo),
+        contentDescription = "로고",
+        modifier = Modifier.size(120.dp),
+        contentScale = ContentScale.Fit
+    )
 }
